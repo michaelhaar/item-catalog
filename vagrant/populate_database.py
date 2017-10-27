66b169bea3c2d103fc09c5bcbd9201aa7f1c1bd7
Michael Haar <michael.haar@gmx.at>
2017-10-27 10:45:04 +0200

    deleted some comments

--- vagrant/populate_database.py
+++ vagrant/populate_database.py
@@ -2,7 +2,7 @@
 # coding=utf8
 
 """
-This file will fill the database with some sample entries if there aren't any
+This file will fill the database with some sample categories if there aren't any
 """
 
 from database_operations import DatabaseOperations
@@ -26,23 +26,3 @@ categories = db.get_categories()
 print("The following categories are now available in the database:")
 for category in categories:
     print("- " + category.title)
-
-
-# # Let's try to add some items to the database, if there aren't any
-# description_placeholder = "Lorem ipsum dolor sit amet, consetetur sadipscing"
-# if db.item_count() == 0:
-#     db.add_item("Stick", description_placeholder, "Hockey")
-#     db.add_item("Goggles", description_placeholder, "Snowboarding")
-#     db.add_item("Snowboard", description_placeholder, "Snowboarding")
-#     db.add_item("Frisbee", description_placeholder, "Frisbee")
-#     db.add_item("Bat", description_placeholder, "Baseball")
-#     db.add_item("Two shinguards", description_placeholder, "Soccer")
-#     db.add_item("Shinguards", description_placeholder, "Soccer")
-#     db.add_item("Jersey", description_placeholder, "Soccer")
-#     db.add_item("Soccer Cleats", description_placeholder, "Soccer")
-#
-# # Print soccer items
-# soccer_items = db.get_items_of_category("Soccer")
-# print("You'll need the following items if you wanna play soccer:")
-# for soccer_item in soccer_items:
-#     print("- " + soccer_item.title)
